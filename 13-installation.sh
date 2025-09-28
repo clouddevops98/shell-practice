e14408deb680358b1cd94f88c24580f25ff5a55f
somayya <somayya.adusumalli98@gmail.com>
2025-09-28 15:11:02 +0530

    session-06

--- 13-installation.sh
+++ 13-installation.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
@@ -5,29 +7,20 @@ if [ $USERID -ne 0 ]; then
     exit 1 # failure is other than 0
 fi
 
-dnf install mysql -y
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: Installing $2 is failure"
+        exit 1
+    else
+        echo "Installing $2 is SUCCESS"
+}
 
-if [ $? -ne 0 ]; then
-    echo "ERROR:: Installing MYSQL is failure"
-    exit 1
-else
-    echo "Installing MYSQL is SUCCESS"
-fi
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
 
 dnf install Nginx -y
-
-if [ $? -ne 0 ]; then
-    echo "ERROR:: Installing Nginx is failure"
-    exit 1
-else
-    echo "Installing Nginx is SUCCESS"
-fi
+VALIDATE $? "Nginx"
 
 dnf install mongodb-mongosh -y
-
-if [ $? -ne 0 ]; then
-    echo "ERROR:: Installing mongodb-mongosh is failure"
-    exit 1
-else
-    echo "Installing mongodb-mongosh is SUCCESS"
-fi
+VALIDATE $? "mongosh"
