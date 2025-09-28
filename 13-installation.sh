e44583c25198e48f115fcf7bd33b017220090af0
somayya <somayya.adusumalli98@gmail.com>
2025-09-28 15:28:20 +0530

    session-06

--- 13-installation.sh
+++ 13-installation.sh
@@ -3,11 +3,11 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run thisb script with root privelege"
+    echo "ERROR:: Please run this script with root privelege"
     exit 1 # failure is other than 0
 fi
 
-VALIDATE(){
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
         echo "ERROR:: Installing $2 is failure"
         exit 1
@@ -15,7 +15,6 @@ VALIDATE(){
         echo "Installing $2 is SUCCESS"
     fi
 }
-
 
 dnf install mysql -y
 VALIDATE $? "MYSQL"
