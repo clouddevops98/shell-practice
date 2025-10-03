3d35b8e82891dab6c534603672beaf60f16f6d62
somayya <somayya.adusumalli98@gmail.com>
2025-10-03 13:13:20 +0530

    session-06

--- 23-while-loop.sh
+++ 23-while-loop.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-count=5
+# count=5
 
-echo "Starting countdown..."
+# echo "Starting countdown..."
 
-while [ $count -gt 0 ]
-do
-  echo "Time left: $count"
-  sleep 1
-  count=$((count - 1))
-done
+# while [ $count -gt 0 ]
+# do
+#   echo "Time left: $count"
+#   sleep 1
+#   count=$((count - 1))
+# done
 
-echo "Times up!"
+# echo "Times up!"
+
+# Read the file line by line
+while IFS= read -r line; do
+    # Process each line here
+    echo "Processing line: $line"
+    # You can perform other commands or operations using the '$line' variable
+done < "20-script-01.sh"
