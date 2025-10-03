1617d216135649a638ff24158b32322c4e46b609
somayya <somayya.adusumalli98@gmail.com>
2025-10-03 20:03:14 +0530

    session-06

--- 25-cpu-usage.sh
+++ 25-cpu-usage.sh
@@ -4,7 +4,7 @@ DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2
 while IFS= read -r line
 do
-    USAGE=$(echo $line | awk '{print $6}') | cut -d "%" -f1)
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
         echo High Usage on $PARTITION: $USAGE"
